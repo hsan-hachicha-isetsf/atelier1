e51ace2586e7dd1e7c1ab7fdf23084466dcfa16e
Hachicha Hassan <hsan.hachicha@gmail.com>
2024-09-11 15:42:22 +0200

    version1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+       <title>Site Ecommercer</title>
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div>Bienvenue dans notre site</div>
+    </body>
+</html>
